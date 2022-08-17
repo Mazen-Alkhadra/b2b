ea4946603b216d59f562342972803e2b395da074
unknown <mazen.alkhadra@outlook.com>
2022-08-18 01:24:40 +0300

    New Publish

--- public/static/js/26.cd7ae32c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpb2b=this.webpackJsonpb2b||[]).push([[26],{965:function(t,e,c){"use strict";c.r(e);c(0);var n=c(742),b=c(741),r=c(732),s=c(1);e.default=Object(r.a)()((function(t){var e=t.t;return Object(s.jsx)(n.a,{textAlign:"center",children:Object(s.jsx)(b.a,{as:"h1",color:"red",content:e("WelcomeToOb")})})}))}}]);
-//# sourceMappingURL=26.cd7ae32c.chunk.js.map
