fb0dc03729c38dccc819823ee5f16c4cefce8310
mazen-khadra <mazen.alkhadra@outlook.com>
2023-10-03 21:06:01 +0400

    New Publish

---
+++ public/static/js/34.f731ef23.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpb2b=this.webpackJsonpb2b||[]).push([[34],{1008:function(t,e,c){"use strict";c.r(e);c(0);var n=c(785),b=c(786),r=c(774),s=c(1);e.default=Object(r.a)()((function(t){var e=t.t;return Object(s.jsx)(n.a,{textAlign:"center",children:Object(s.jsx)(b.a,{as:"h1",color:"red",content:e("WelcomeToOb")})})}))}}]);
+//# sourceMappingURL=34.f731ef23.chunk.js.map
